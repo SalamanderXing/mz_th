a9b8e017e18e19077355aa33df85298043de3fe2
salamandercrossing <giulio.zani@gmail.com>
2023-08-30 16:06:31 +0000

    update

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
-df = pd.read_parquet('../mini_arg/01844147-17ff-4335-8186-c10c2b989260/scenario_01844147-17ff-4335-8186-c10c2b989260.parquet')
-print(df)
+df = pd.read_parquet(
+    "../train/01844147-17ff-4335-8186-c10c2b989260/scenario_01844147-17ff-4335-8186-c10c2b989260.parquet"
+)
 
+print(df)
