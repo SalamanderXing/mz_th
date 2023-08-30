5349c4e2355554337ae8e8660964a8d4dffb7898
salamandercrossing <giulio.zani@gmail.com>
2023-08-30 16:08:01 +0000

    update

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 df = pd.read_parquet(
-    "../train/01844147-17ff-4335-8186-c10c2b989260/scenario_01844147-17ff-4335-8186-c10c2b989260.parquet"
+    "./train/01844147-17ff-4335-8186-c10c2b989260/scenario_01844147-17ff-4335-8186-c10c2b989260.parquet"
 )
 
 print(df)
