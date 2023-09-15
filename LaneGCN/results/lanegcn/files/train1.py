d37aae699a28c152d1d8fe270bfe2e339fdfdc09
salamandercrossing <giulio.zani@gmail.com>
2023-09-15 15:03:19 +0000

    update

--- LaneGCN/results/lanegcn/files/train1.py
+++ LaneGCN/results/lanegcn/files/train1.py
@@ -1,13 +1,5 @@
-# Copyright (c) 2020 Uber Technologies, Inc.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import os
 
-os.umask(0)
-os.environ["MKL_NUM_THREADS"] = "1"
-os.environ["NUMEXPR_NUM_THREADS"] = "1"
-os.environ["OMP_NUM_THREADS"] = "1"
 import argparse
 import numpy as np
 import random
@@ -17,23 +9,13 @@ import shutil
 from importlib import import_module
 from numbers import Number
 
-from tqdm import tqdm
 import torch
 from torch.utils.data import Sampler, DataLoader
 
-# import horovod.torch as hvd
-
-
-from torch.utils.data.distributed import DistributedSampler
 
 from utils import Logger, load_pretrain
 
-from mpi4py import MPI
 
-
-comm = MPI.COMM_WORLD
-# hvd.init()
-# torch.cuda.set_device(hvd.local_rank())
 
 root_path = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, root_path)
@@ -41,7 +23,7 @@ sys.path.insert(0, root_path)
 
 parser = argparse.ArgumentParser(description="Fuse Detection in Pytorch")
 parser.add_argument(
-    "-m", "--model", default="lanegcn", type=str, metavar="MODEL", help="model name"
+    "-m", "--model", default="model", type=str, metavar="MODEL", help="model name"
 )
 parser.add_argument("--eval", action="store_true")
 parser.add_argument(
@@ -53,7 +35,7 @@ parser.add_argument(
 
 
 def main():
-    seed = 32  # hvd.rank()
+    seed = 0
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
     np.random.seed(seed)
@@ -63,12 +45,6 @@ def main():
     args = parser.parse_args()
     model = import_module(args.model)
     config, Dataset, collate_fn, net, loss, post_process, opt = model.get_model()
-
-    if config["horovod"]:
-        # opt.opt = hvd.DistributedOptimizer(
-        #     opt.opt, named_parameters=net.named_parameters()
-        # )
-        pass
 
     if args.resume or args.weight:
         ckpt_path = args.resume or args.weight
@@ -83,26 +59,23 @@ def main():
     if args.eval:
         # Data loader for evaluation
         dataset = Dataset(config["val_split"], config, train=False)
-        # val_sampler = DistributedSampler(
-        #     dataset, num_replicas, rank=hvd.rank()
-        # )
+     
         val_loader = DataLoader(
             dataset,
             batch_size=config["val_batch_size"],
             num_workers=config["val_workers"],
             # sampler=val_sampler,
+            shuffle=True,
             collate_fn=collate_fn,
             pin_memory=True,
         )
 
-        # hvd.broadcast_parameters(net.state_dict(), root_rank=0)
         val(config, val_loader, net, loss, post_process, 999)
         return
 
     # Create log and copy all code
     save_dir = config["save_dir"]
     log = os.path.join(save_dir, "log")
-    # if hvd.rank() == 0:
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
     sys.stdout = Logger(log)
@@ -118,14 +91,11 @@ def main():
 
     # Data loader for training
     dataset = Dataset(config["train_split"], config, train=True)
-    train_sampler = DistributedSampler(
-        dataset,
-    )
     train_loader = DataLoader(
         dataset,
         batch_size=config["batch_size"],
         num_workers=config["workers"],
-        sampler=train_sampler,
+        shuffle=True,
         collate_fn=collate_fn,
         pin_memory=True,
         worker_init_fn=worker_init_fn,
@@ -134,18 +104,15 @@ def main():
 
     # Data loader for evaluation
     dataset = Dataset(config["val_split"], config, train=False)
-    val_sampler = DistributedSampler(dataset, num_replicas=hvd.size(), rank=hvd.rank())
     val_loader = DataLoader(
         dataset,
         batch_size=config["val_batch_size"],
         num_workers=config["val_workers"],
-        sampler=val_sampler,
+        shuffle=True,
         collate_fn=collate_fn,
         pin_memory=True,
     )
 
-    # hvd.broadcast_parameters(net.state_dict(), root_rank=0)
-    # hvd.broadcast_optimizer_state(opt.opt, root_rank=0)
 
     epoch = config["epoch"]
     remaining_epochs = int(np.ceil(config["num_epochs"] - epoch))
@@ -154,10 +121,9 @@ def main():
 
 
 def worker_init_fn(pid):
-    # np_seed = hvd.rank() * 1024 + int(pid)
-    np_seed = 32
+    np_seed = int(pid)
     np.random.seed(np_seed)
-    random_seed = np.random.randint(2**32 - 1)
+    random_seed = np.random.randint(2 ** 32 - 1)
     random.seed(random_seed)
 
 
@@ -168,12 +134,14 @@ def train(epoch, config, train_loader, net, loss, post_process, opt, val_loader=
     num_batches = len(train_loader)
     epoch_per_batch = 1.0 / num_batches
     save_iters = int(np.ceil(config["save_freq"] * num_batches))
-    display_iters = int(config["display_iters"] / (config["batch_size"]))
+    display_iters = int(
+        config["display_iters"] / (config["batch_size"])
+    )
     val_iters = int(config["val_iters"] / (config["batch_size"]))
 
     start_time = time.time()
     metrics = dict()
-    for i, data in tqdm(enumerate(train_loader)):
+    for i, data in enumerate(train_loader):
         epoch += epoch_per_batch
         data = dict(data)
 
@@ -187,12 +155,12 @@ def train(epoch, config, train_loader, net, loss, post_process, opt, val_loader=
         lr = opt.step(epoch)
 
         num_iters = int(np.round(epoch * num_batches))
-
-        save_ckpt(net, opt, config["save_dir"], epoch)
+        if num_iters % save_iters == 0 or epoch >= config["num_epochs"]:
+            save_ckpt(net, opt, config["save_dir"], epoch)
 
         if num_iters % display_iters == 0:
             dt = time.time() - start_time
-            metrics = sync(metrics)
+            # metrics = sync(metrics)
             post_process.display(metrics, dt, epoch, lr)
             start_time = time.time()
             metrics = dict()
@@ -219,7 +187,8 @@ def val(config, data_loader, net, loss, post_process, epoch):
             post_process.append(metrics, loss_out, post_out)
 
     dt = time.time() - start_time
-    metrics = sync(metrics)
+    # metrics = sync(metrics)
+    # if hvd.rank() == 0:
     post_process.display(metrics, dt, epoch)
     net.train()
 
@@ -234,22 +203,10 @@ def save_ckpt(net, opt, save_dir, epoch):
 
     save_name = "%3.3f.ckpt" % epoch
     torch.save(
-        {"epoch": epoch, "state_dict": state_dict, "opt_state": opt.opt.state_dict()},
+        {"epoch": epoch, "state_dict": state_dict, "opt_state": opt.state_dict()},
         os.path.join(save_dir, save_name),
     )
 
-
-def sync(data):
-    data_list = comm.allgather(data)
-    data = dict()
-    for key in data_list[0]:
-        if isinstance(data_list[0][key], list):
-            data[key] = []
-        else:
-            data[key] = 0
-        for i in range(len(data_list)):
-            data[key] += data_list[i][key]
-    return data
 
 
 if __name__ == "__main__":
