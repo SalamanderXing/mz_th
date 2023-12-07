15879212a6cd1707218f74b671b9d91d8ad1cee5
salamandercrossing <giulio.zani@gmail.com>
2023-12-07 19:22:35 +0000

    update

--- create_video.py
+++ create_video.py
@@ -1,13 +1,13 @@
 import os
 
-input_folder = "../dataset/train"
-output_folder = "../visualizations/"
+input_folder = "./dataset/train_subset"
+output_folder = "./visualizations/"
 
 cmd = f"""
 python "./avi_scripts/generate_forecasting_scenario_visualizations.py" \
     --argoverse-scenario-dir={input_folder} \
     --viz-output-dir={output_folder} \
-    --num-scenarios=2 \
+    --num-scenarios=4 \
 """
 print(cmd)
 os.system(cmd)
